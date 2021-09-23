9483c136f29d531c6404bef678b9c32b2d7032e6
ReyesNicos <75365528+ReyesNicos@users.noreply.github.com>
2021-09-23 16:39:34 +0800

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -57,12 +57,12 @@ class Quiz {
         //   fill("red");
         // }
 
-         if (correctAns === allContestants[plr].answer){
-           fill("Green")
-         }
-         else{
-           fill("red");
-        }
+         //if (correctAns === allContestants[plr].answer){
+         //  fill("Green")
+         //}
+         //else{
+         //  fill("red");
+         // }
 
         // if (correctAns === allContestants[plr].answer){
         //   fill("red")
@@ -71,12 +71,12 @@ class Quiz {
         //   fill("green");
         // }
 
-        // if (correctAns = allContestants[plr].answer){
-        //   fill("Green")
-        // }
-        // else{
-        //   fill("red");
-        // }
+         if (correctAns = allContestants[plr].answer){
+           fill("Green")
+         }
+         else{
+           fill("red");
+         }
 
         display_Answers+=30;
         textSize(20);
